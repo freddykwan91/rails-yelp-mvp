916103087cae4a2cb793f5f81257c98ac191db1c
Freddy Kurniawan <freddykwan.work@gmail.com>
2018-02-17 15:34:13 +0800

    create views from restaurant

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'restaurants/index'
-
-  get 'restaurants/new'
-
-  get 'restaurants/show'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :restaurants, only: [:index, :new, :create, :show]
 end
